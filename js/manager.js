01fa6d8a6b553ea84ddb0a79a9570dae00018836
Italo Costa <italo.costa.ce@gmail.com>
2017-11-05 03:55:43 +0000

    Update manager.js

--- js/manager.js
+++ js/manager.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var socket = io.connect("http://192.168.0.12:3000");
+    var socket = io.connect("https://countdown-manager.herokuapp.com:3000");
 	var $clock = $('#clock');
 	var h = $('#hor').val();
 	var m = $('#min').val();
